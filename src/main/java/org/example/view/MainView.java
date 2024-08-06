db4a87a172ea5330bdb0bc4ca79ebcded8f4220f
Bruno Corrêa <correaricardobruno@gmail.com>
2024-08-05 22:35:31 -0300

    wip

---
+++ src/main/java/org/example/view/MainView.java
@@ -0,0 +1,9 @@
+package org.example.view;
+
+import javax.swing.*;
+
+public class MainView {
+    private JTextField clientTextField;
+    private JLabel clientLabel;
+    private JLabel selectedAnimalPanelTitle;
+}
