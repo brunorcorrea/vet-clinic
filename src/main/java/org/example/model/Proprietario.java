7ad1502ffd5e632b4dbb5316fd1fccd89a010e19
Bruno Correa <correaricardobruno@gmail.com>
2024-09-18 21:11:26 -0300

    wip

---
+++ src/main/java/org/example/model/Proprietario.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+public class Proprietario {
+
+    private int id;
+    private String cpf;
+    private String nomeCompleto;
+    private String endereco;
+
+
+}
--- src/main/java/org/example/view/MainView.java
+++
@@ -1,9 +0,0 @@
-package org.example.view;
-
-import javax.swing.*;
-
-public class MainView {
-    private JTextField clientTextField;
-    private JLabel clientLabel;
-    private JLabel selectedAnimalPanelTitle;
-}
