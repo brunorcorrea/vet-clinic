c21bcda491c91c4a1e1f91767140898ef3245bbc
Bruno Correa <correaricardobruno@gmail.com>
2024-09-27 14:19:02 -0300

    wip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.view.MainView;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Main View");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setContentPane(new MainView().getMainPanel());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
---
+++ src/main/java/org/example/view/MainView.java
@@ -0,0 +1,11 @@
+package org.example.view;
+
+import javax.swing.*;
+
+public class MainView {
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
