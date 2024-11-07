7c8cdbd57f371868b32960def9a7edbe10efe73e
Bruno Correa <correaricardobruno@gmail.com>
2024-11-06 23:10:11 -0300

    fix veterinario layout

--- src/main/java/org/example/view/VeterinarioView.java
+++ src/main/java/org/example/view/VeterinarioView.java
@@ -153,7 +153,7 @@ public class VeterinarioView {
         filtroNomeTextField = new JTextField();
         panel3.add(filtroNomeTextField, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JScrollPane scrollPane1 = new JScrollPane();
-        panel1.add(scrollPane1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        panel1.add(scrollPane1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         veterinarioTable = new JTable();
         veterinarioTable.setAutoResizeMode(2);
         scrollPane1.setViewportView(veterinarioTable);
