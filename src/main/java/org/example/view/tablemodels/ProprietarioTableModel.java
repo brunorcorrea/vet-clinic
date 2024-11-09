ee11e40f0fff960f0f1b2d7de20708a86449def0
Bruno Correa <correaricardobruno@gmail.com>
2024-11-08 22:09:14 -0300

    test and fix proprietario

--- src/main/java/org/example/view/tablemodels/ProprietarioTableModel.java
+++ src/main/java/org/example/view/tablemodels/ProprietarioTableModel.java
@@ -45,9 +45,41 @@ public class ProprietarioTableModel extends GenericTableModel {
         Proprietario proprietario = (Proprietario) vDados.get(rowIndex);
 
         switch (columnIndex) {
-            case 1 -> proprietario.setNomeCompleto((String) aValue);
-            case 2 -> proprietario.setTelefone((String) aValue);
-            case 3 -> proprietario.setEndereco((String) aValue);
+            case 1 -> {
+                String nomeCompleto = (String) aValue;
+
+                if (nomeCompleto.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Nome completo não pode ser vazio", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                proprietario.setNomeCompleto(nomeCompleto);
+            }
+            case 2 -> {
+                String telefone = (String) aValue;
+
+                if (telefone.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Telefone não pode ser vazio", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                if (!telefone.matches("\\(\\d{2}\\)\\s\\d{5}-\\d{4}")) {
+                    JOptionPane.showMessageDialog(null, "Telefone deve estar no formato (XX) XXXXX-XXXX", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                proprietario.setTelefone(telefone);
+            }
+            case 3 -> {
+                String endereco = (String) aValue;
+
+                if (endereco.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Endereço não pode ser vazio", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                proprietario.setEndereco(endereco);
+            }
             default -> throw new IndexOutOfBoundsException("columnIndex out of bounds");
         }
 
